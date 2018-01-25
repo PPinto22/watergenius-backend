7253540a067af9f130cedfe9263412e62d60ab7a
Pedro Pinto <pedropinto24@hotmail.com>
2018-01-25 18:19:32 +0000

    Accept read timestamps defined in the POST request

--- watergenius/api/models/reads.py
+++ watergenius/api/models/reads.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils.timezone import now
 
 from api.models.sensors import Sensor
 
 
 class Read(models.Model):
     read_id = models.AutoField(primary_key=True)
-    read_timestamp = models.DateTimeField(auto_now=True)
+    read_timestamp = models.DateTimeField(default=now)
     read_value = models.FloatField()
     read_sensor = models.ForeignKey(Sensor, related_name='belongs_to_sensor', on_delete=models.CASCADE)
