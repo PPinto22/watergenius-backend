bd8cc11a63d3da18e59a6228f6a657efaf5c49bd
Pedro Pinto <pedropinto24@hotmail.com>
2018-01-31 11:53:56 +0000

    Dayplan rain

--- api/models/plans.py
+++ api/models/plans.py
@@ -8,5 +8,7 @@ class DayPlan(models.Model):
     dayplan_gen_time = models.DateTimeField(auto_now=True)
     dayplan_time = models.DateTimeField()
     dayplan_water_qty = models.FloatField()
-    dayplan_water_qty_unit = models.CharField(max_length=10, default='L')
+    dayplan_water_qty_unit = models.CharField(max_length=6, default='mm')
+    dayplan_rain = models.FloatField(default=0.0)
+    dayplan_rain_unit = models.CharField(max_length=6, default='mm')
     dayplan_sub = models.ForeignKey(SubSpace, related_name='belongs_to_subspace', on_delete=models.CASCADE)
--- api/serializers/plans.py
+++ api/serializers/plans.py
@@ -4,11 +4,13 @@ from api.models import DayPlan
 
 
 class DayPlanSerializer(serializers.ModelSerializer):
-    dayplan_water_qty_unit = serializers.CharField(max_length=10, read_only=True)
+    dayplan_water_qty_unit = serializers.CharField(max_length=6, read_only=True)
+    dayplan_rain_unit = serializers.CharField(max_length=6, read_only=True)
 
     class Meta:
         model = DayPlan
         fields = ('dayplan_id', 'dayplan_gen_time', 'dayplan_time',
                   'dayplan_water_qty', 'dayplan_water_qty_unit',
+                  'dayplan_rain', 'dayplan_rain_unit',
                   'dayplan_sub')
         validators = []  # Remove a default "unique together" constraint.
